979ca5e0172c38d282c44d77de642b075f0cc3fa
Aaron <proaaron.714@gmail.com>
2019-04-18 13:21:05 -0400

    Update Hamburger Menu

--- js/toggle.js
+++ js/toggle.js
@@ -1,9 +1,17 @@
-function myFunction(x) {
-  x.classList.toggle("change");
-  var x = document.getElementById("links");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+// function myFunction(x) {
+//   x.classList.toggle("change");
+//   var x = document.getElementById("links");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+var links = document.getElementById("links");
+var hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", function(){
+  hamburger.classList.toggle("change");
+  links.classList.toggle("nav_mobile");
+});
